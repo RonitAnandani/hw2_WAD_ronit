021b6bebc27a699af9dd416632ef0b0670d43dd4
RONIT ANANDANI <95768146+RonitAnandani@users.noreply.github.com>
2022-03-25 20:54:52 +0300

    Update loginsystem.py

--- loginsystem.py
+++ loginsystem.py
@@ -16,6 +16,64 @@ def index():
 
     return render_template('login.html')
 
+
+# ------------------------------------Image Importing-----------------------------------------------
+
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+ 
+
+app.secret_key = "secret key" 
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def home():
+    if 'username' in session:
+        return render_template('index.html')
+
+    return render_template('register.html')
+    
+ 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/upload/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+ 
+
+#----------------------------------------------image part over------------------------------------------------
+
+
+
+
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
